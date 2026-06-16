f3f7d17d2341dadb3fb88f12bc740213d3adad78
ANGRYCONE <theepicmanofficial@gmail.com>
2026-06-16 15:22:20 +1000

    Update Bearder to version 1.2.4

--- Formula/bearder.rb
+++ Formula/bearder.rb
@@ -1,9 +1,9 @@
 class Bearder < Formula
   desc "A custom CLI tool (Bearder)"
   homepage "https://github.com/ANGRYCONE/bearder7"
-  url "https://github.com/ANGRYCONE/homebrew-bearder7/releases/download/v1.2.3/bearder"
-  sha256 "f15e351ee3fbb820cb35df596998e3c96323b46edced633a617214389ede5468"
-  version "1.2.3"
+  url "https://github.com/ANGRYCONE/homebrew-bearder7/releases/download/v1.2.4/bearder"
+  sha256 "39b3ab7d81844163c50cd25055f2a9b8dc01a57492b6e2eb6da0e958a5b412ae"
+  version "1.2.4"
 
   depends_on :macos
 
